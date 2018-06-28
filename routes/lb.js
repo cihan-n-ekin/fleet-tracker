e3ad10756b65b9ba4c0c05c37a3a231c67105205
Ekin Oral <oralekin@gmail.com>
2018-06-28 07:24:51 +0000

    endpoint for locationbox apis

---
+++ routes/lb.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const request = require("request");
+
+// GET from LocationBox Api's
+router.get('/:api', function (req, res, next) {
+	console.log(req.url);
+	const apiurl = `http://www.locationbox.com.tr/locationbox/services?Cmd=${req.params.api}&Typ=JSON&Key=${global.key}&${req.url.split("?")[1]}`
+	request(apiurl, function (err, resp, body) {
+		console.log(apiurl);
+		if (err) {
+			console.error(err);
+			res.statusCode(404);
+			next();
+		} else {
+			res.send(body);
+		}
+	});
+});
+
+module.exports = router;
