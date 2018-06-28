2c4c2b72e45285e405f7cefd0319fc27bf930016
Cihan Bosnalı <cihanbosna@hotmail.com>
2018-06-28 09:47:13 +0300

    openmenu added

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,12 @@
-function openNav(x){
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav(x) {
     x.classList.toggle("change");
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
